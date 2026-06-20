2a92a688c467f847de2660ef17175e2213831496
ramaa <rama.arzamendia@gmail.com>
2026-06-20 14:27:51 -0300

    modificacion estructura NodoAVL

--- src/tda/NodoAVL.java
+++ src/tda/NodoAVL.java
@@ -2,12 +2,14 @@ package tda;
 
 public class NodoAVL {
     int dato;
+    int cantidad;
     int altura;
     NodoAVL izquierdo;
     NodoAVL derecho;
 
     public NodoAVL(int dato) {
         this.dato = dato;
+        this.cantidad = 1;
         this.altura = 1;
         this.izquierdo = null;
         this.derecho = null;
