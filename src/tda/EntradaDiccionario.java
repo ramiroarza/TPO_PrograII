110c1deb99fd1972be5398f4a7ec64d3f29bb577
ramaa <rama.arzamendia@gmail.com>
2026-06-20 16:25:18 -0300

    modificacion metodo constructor entradaDiccionario

--- src/tda/EntradaDiccionario.java
+++ src/tda/EntradaDiccionario.java
@@ -3,9 +3,11 @@ package tda;
 public class EntradaDiccionario<T> {
     ClaveProducto clave;
     T valor;
+    EntradaDiccionario<T> siguiente;
 
     public EntradaDiccionario(ClaveProducto clave, T valor) {
         this.clave = clave;
         this.valor = valor;
+        this.siguiente = null;
     }
 }
