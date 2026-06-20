79a09e56be364ac88437052e01c1feb157623fba
ramaa <rama.arzamendia@gmail.com>
2026-06-20 17:08:16 -0300

    metodo constructor agregado a la clase arista

--- src/tda/Arista.java
+++ src/tda/Arista.java
@@ -1,4 +1,14 @@
 package tda;
 
 public class Arista {
+    int destino;
+    int peso;
+
+    public Arista(int destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public int getDestino() { return destino; }
+    public int getPeso() { return peso; }
 }
